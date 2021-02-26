146fcc6478470ca2ee35b05bf90850ab1867998e
Martin Lukasik <martilukas7@gmail.com>
2021-02-26 21:03:22 +0100

    resp

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, world!")
+		return c.SendString(os.Getenv("MYSQL_CONN_STR"))
 	})
 
 	app.Listen(":8000")
