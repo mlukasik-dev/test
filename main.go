866b04bfbf005f7fe139961a83ac0d384d3e1b9a
Martin Lukasik <martilukas7@gmail.com>
2021-02-26 18:04:04 +0100

    new route

--- main.go
+++ main.go
@@ -5,5 +5,9 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, world!")
+	})
+
 	app.Listen(":8000")
 }
